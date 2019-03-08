28009635dd90a6af22c3817ff5061b30835b1850
restot <92ilya.icom@gmail.com>
2019-03-08 13:16:43 -0500

    products_show_ajax  19-03-08 13:16:43

--- app/overrides/add_ajax_add_to_cart_button.rb
+++ app/overrides/add_ajax_add_to_cart_button.rb
@@ -1,76 +1,24 @@
 Deface::Override.new(virtual_path: 'spree/products/_product',
   name: 'add_add_to_cart_button_to_products_page',
   insert_after: '.panel-footer',
-  text: <<-eos
-    <div class="add_to_cart_products__panel">
-      <%= form_for :order, :url => populate_orders_path, html: { class: 'add_to_cart_products__form' } do |f| %>
-        <%= hidden_field_tag "variant_id", product.master.id %>
-        <%= hidden_field_tag :back, true %>
-        <% if product.price_in(current_currency) and !product.price.nil? %>
-          <% if product.variants.size > 1 || product.master.can_supply? %>
-            <div class="add-to-cart">
-              <div class="input-group">
-                <%= number_field_tag :quantity, 1, class: 'title form-control', min: 1, step: 1 %>
-                <span class="input-group-btn">
-                  <%= button_tag :class => 'btn btn-success add_to_cart_products__button', :type => :submit do %>
-                    + <span class="glyphicon glyphicon-shopping-cart"></span>
-                  <% end %>
-                </span>
-              </div>
-            </div>
-          <% else %>
-            <div class="out-of-stock"><%= Spree.t(:out_of_stock) %></div>
-          <% end %>
-        <% else %>
-          <div id="product-price">
-            <div><span class="price selling" itemprop="price"><%= Spree.t('product_not_available_in_this_currency') %></span></div>
-          </div>
-        <% end %>
-      <% end %>
-      <script>
-        $(function() {
-          $('.add_to_cart_products__form').addToCartAjaxForm();
-        });
-      </script>
-    </div>
-  eos
+  partial: 'spree/overrides/add_to_cart_panel'
 )
 
-Deface::Override.new(virtual_path: 'spree/products/_cart_form',
+Deface::Override.new(virtual_path: 'spree/products/show',
                      name: 'add_ajax_form_to_product_page',
-                     replace: '.add-to-cart',
-                     text: <<-eos
-    <div id="show_fix" class="add_to_cart_products__panel">
-      <%= form_for :order, :url => populate_orders_path, html: { class: 'add_to_cart_products__form' } do |f| %>
-        <%= hidden_field_tag "variant_id", @product.master.id %>
-        <%= hidden_field_tag :back, true %>
-        <% if @product.price_in(current_currency) and !@product.price.nil? %>
-          <% if @product.variants.size > 1 || @product.master.can_supply? %>
-            <div class="add-to-cart">
-              <div class="input-group">
-                <%= number_field_tag :quantity, 1, class: 'title form-control', min: 1, step: 1 %>
-                <span class="input-group-btn">
-                  <%= button_tag :class => 'btn btn-success add_to_cart_products__button', :type => :submit do %>
-                    Добавить в корзину + <span class="glyphicon glyphicon-shopping-cart"></span>
-                  <% end %>
-                </span>
-              </div>
-            </div>
-          <% else %>
-            <div class="out-of-stock"><%= Spree.t(:out_of_stock) %></div>
-          <% end %>
-        <% else %>
-          <div id="product-price">
-            <div><span class="price selling" itemprop="price"><%= Spree.t('product_not_available_in_this_currency') %></span></div>
-          </div>
-        <% end %>
-      <% end %>
-      <script>
-        $(function() {
-          $('.add_to_cart_products__form').addToCartAjaxForm();
-        });
-      </script>
-    </div>
-eos
+                     replace: '#cart-form',
+                     partial: 'spree/overrides/add_to_cart_product_show'
+                     # disabled: true
 )
 
+# Deface::Override.new(virtual_path: 'spree/products/show',
+#                      name: 'add_ajax_form_to_product_page',
+#                      insert_after: '#cart-form',
+#                      text: <<-eos
+#     <script>
+#       $(function() {
+#         $('#cart-form form').addToCartAjaxForm();
+#       });
+#     </script>
+# eos
+# )
