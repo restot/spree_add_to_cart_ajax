d4b7da899676de8acecac5f5644892e36559244a
restot <92ilya.icom@gmail.com>
2019-04-08 03:33:59 -0400

    fix js for dark theme  19-04-08 03:33:59

--- app/assets/javascripts/spree/frontend/spree_add_to_cart_ajax.js
+++ app/assets/javascripts/spree/frontend/spree_add_to_cart_ajax.js
@@ -4,47 +4,59 @@
 
 (function($) {
 
-  $.fn.addToCartAjaxForm = function() {
-    var options = {
-      dataType: 'json',
-      success: function(e) {
-        addAlert('success', e.message);
-        $.ajax({ url: Spree.pathFor("cart_link"), success: function(data) {
-          $('#link-to-cart').html(data);
-        }});
-      },
-      error: function(e) {
-        addAlert('error', JSON.parse(e.responseText).message);
-      }
-    };
-    return this.ajaxForm(options);
-  }
-
-  function animate($elem, action, speed, callback) {
-    $elem.animate({
-      height: action,
-      marginBottom: action,
-      marginLeft: action,
-      marginRight: action,
-      marginTop: action,
-      opacity: action,
-      paddingBottom: action,
-      paddingLeft: action,
-      paddingRight: action,
-      paddingTop: action}, speed, callback);
-  }
-
-  function animateAlerts($alerts) {
-    if ($alerts.length > 2) {
-      animate($alerts.last(), 'hide', 200, function() { $(this).remove(); });
+    $.fn.addToCartAjaxForm = function() {
+        var options = {
+            dataType: 'json',
+            success: function(e) {
+                addAlert('success', e.message);
+                $.ajax({ url: Spree.pathFor("cart_link"), success: function(data) {
+                        var regex = /\(\d+\)/g;
+                        var found = data.match(regex);
+                        $('#badge-to-cart').html(found);
+                    }});
+            },
+            error: function(e) {
+                addAlert('error', JSON.parse(e.responseText).message);
+            }
+        };
+        return this.ajaxForm(options);
     }
-    animate($alerts.first(), 'show', 240);
-  }
+    $.fn.fetch_cart = function () {
+        return $.ajax({
+            url: Spree.pathFor('/cart_link'),
+            cache: false
+        }).done(function (data) {
+            var regex = /\(\d+\)/g;
+            var found = data.match(regex);
+            $('#badge-to-cart').html(found);
+        })}
 
-  function addAlert(type, message) {
-    var $content = $('#content');
-    $content.prepend('<div style="display: none" class="alert alert-' + type + '">' + message + '</div>');
-    animateAlerts($content.find('.alert'));
-  }
+    function animate($elem, action, speed, callback) {
+        $elem.animate({
+            height: action,
+            marginBottom: action,
+            marginLeft: action,
+            marginRight: action,
+            marginTop: action,
+            opacity: action,
+            paddingBottom: action,
+            paddingLeft: action,
+            paddingRight: action,
+            paddingTop: action}, speed, callback);
+    }
+
+    function animateAlerts($alerts) {
+        if ($alerts.length > 2) {
+            animate($alerts.last(), 'hide', 200, function() { $(this).remove(); });
+        }
+        animate($alerts.first(), 'show', 240);
+    }
+
+    function addAlert(type, message) {
+        var $content = $('#content');
+        $content.prepend('<div style="display: none" class="alert alert-' + type + '">' + message + '</div>');
+        animateAlerts($content.find('.alert'));
+    }
 
 }(jQuery));
+
